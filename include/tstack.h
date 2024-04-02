de19b46a7bb5ae3bc456cf1f1152c6c2889e198a
Tsvetov1984 <acvetov68@gmail.com>
2024-04-02 19:34:37 +0300

    Asd-5

--- include/tstack.h
+++ include/tstack.h
@@ -6,55 +6,37 @@
 
 template<typename T, int size>
 class TStack {
-private:
-
-    T *data;
+    private:
+    T data[100];
     int top;
 
-    void resize(int nsize) {
-        T *temp = new T[nsize];
-        for (int i = 0; i < size; ++i) {
-            temp[i] = data[i];
-        }
-        delete[] data;
-        data = temp;
-        nsize = size;
+    public:
+    TStack() : top(-1) {}
+    void push(T value) {
+        if (!isFull())
+            data[++top] = value;
+        else
+            throw std::string("Full");
     }
 
-public:
-
-    TStack() {
-        data = new T[size];
-        top = 0;
-    }
-
-    ~TStack() {
-        delete[] data;
-    }
-
-    void push(T item) {
-        if (top == size) {
-            resize(2 * size);
-        }
-        data[top++] = item;
+    T get() const {
+        return data[top];
     }
 
     T pop() {
-        if (top > 0) {
-            return data[--top];
-        } else {
+        if (isEmpty()) {
             throw std::string("Empty");
+        } else {
+            return data[top--];
         }
     }
 
-    bool isEmpty() {
-        return top == 0;
+    bool isEmpty() const {
+        return top == -1;
     }
 
-    T get() {
-        if (!isEmpty()) {
-            return data[top - 1];
-        }
+    bool isFull() const {
+        return top == size - 1;
     }
 };
 
