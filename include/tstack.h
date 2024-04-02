05f0ad7d2ac5b44c0e0c90ba15b37f4c369f77b0
Tsvetov1984 <acvetov68@gmail.com>
2024-04-02 18:12:55 +0300

    ASD-5
    .

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,54 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T *data;
+    int top;
+
+    void resize(int nsize) {
+        T *temp = new T[nsize];
+        for (int i = 0; i < size; ++i) {
+            temp[i] = data[i];
+        }
+        delete[] data;
+        data = temp;
+        nsize = size;
+    }
+
+public:
+    TStack() {
+        data = new T[size];
+        top = 0;
+    }
+
+    ~TStack() {
+        delete[] data;
+    }
+
+    void push(T item) {
+        if (top == size) {
+            resize(2 * size);
+        }
+        data[top++] = item;
+    }
+
+    T pop() {
+        if (top > 0) {
+            return data[--top];
+        } else {
+            throw std::string("Empty");
+        }
+    }
+
+    bool isEmpty() {
+        return top == 0;
+    }
+
+    T get() {
+        if (!isEmpty()) {
+            return data[top - 1];
+        }
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,79 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int GetPrioriti(char operand) {
+    if (operand == '+' || operand == '-') return 1;
+    if (operand == '*' || operand == '/') return 2;
+    else return 0;
 }
 
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
+bool ProverkaNaOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/');
+}
+
+std::string infx2pstfx(std::string inf) {
+    std::string postfix;
+    TStack<char, 100> stack;
+    for (char c: inf) {
+        if (isdigit(c)) {
+            postfix += c;
+            postfix += ' ';
+        } else if (c == '(') {
+            stack.push(c);
+        } else if (ProverkaNaOperator(c)) {
+            while (!stack.isEmpty() && GetPrioriti(c) > GetPrioriti(stack.get())) {
+                postfix += stack.get();
+                postfix += ' ';
+                stack.push(c);
+            }
+            stack.get();
+        } else if (c == ')') {
+            while (!stack.isEmpty() && stack.get() != '(') {
+                postfix += stack.get();
+                postfix += ' ';
+                stack.pop();
+            }
+            stack.pop();
+        }
+    }
+    while (!stack.isEmpty()) {
+        postfix += stack.get();
+        postfix += ' ';
+        stack.pop();
+    }
+    return postfix;
+}
+
+int eval(std::string post) {
+    TStack<int, 100> stack;
+    std::string Number;
+    for (char c: post) {
+        if (isdigit(c)) {
+            Number += c;
+        } else if (Number != "") {
+            stack.push(std::stoi(Number));
+            Number = "";
+        }
+        if (ProverkaNaOperator(c)) {
+            int op2 = stack.get();
+            stack.pop();
+            int op1 = stack.get();
+            stack.pop();
+            switch (c) {
+                case '+':
+                    stack.push(op1 + op2);
+                    break;
+                case '-':
+                    stack.push(op1 - op2);
+                    break;
+                case '*':
+                    stack.push(op1 * op2);
+                    break;
+                case '/':
+                    stack.push(op1 / op2);
+                    break;
+            }
+        }
+    }
+    return stack.get();
 }
